9b3070892b78a8aedb36c1adae38c44b7c2eac5e
[PeterWolf242] <[peter.ebm@web.de]>
2024-12-09 15:01:07 +0100

    String-Methods-Level-1_11

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+let x = "Hallo";
+
+let y = "World";
+
+z = x + y;
+
+document.write(z);
+
+let n = x + " " + y;
+
+document.write(n);
+
+x+="World";
+
+document.write(x);
+
+let meinString = "Ich bin Erst:r";
+
+let neuerString = meinString + " Ich komme auf Platz zwei";
+
+console.log(neuerString);
